63b69f1d863a80b8e2f09aef6a5f32679159c01a
jarviswynn123 <jmwynn11998@gmail.com>
2023-11-05 11:56:04 -0500

    docstrings added to functions

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ color_list = []
 
 
 def create_list_of_colors(lst):
+    """Create random list of colors using faker
+
+    Args:
+        lst: Any list you want to add colors too
+
+    Returns:
+        Random List of colors
+    """
     for i in range(20):
         lst.append(fake.color_name())
     return lst
@@ -19,6 +27,14 @@ create_list_of_colors(color_list)
 # print(color_list)
 
 def remove_duplicates(lst):
+    """Remove duplicates from a list
+
+    Args:
+        lst: Any list you want duplicates removed from
+
+    Returns:
+        set: returns a set with no duplicates
+    """
     lst = str(set(lst))
     return lst
 # print(remove_duplicates(color_list))
